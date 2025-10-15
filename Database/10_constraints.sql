438f8ea780328918e66fd803887049a53904ba65
Meisam Ghasemi <meisamghasemi@ymail.com>
2025-10-15 15:34:13 +0330

    SQL constraints commands

---
+++ Database/10_constraints.sql
@@ -0,0 +1,53 @@
+CREATE TABLE contacts (
+	name VARCHAR(100) NOT NULL,
+    phone VARCHAR(15) NOT NULL UNIQUE
+);
+
+INSERT INTO contacts (name, phone)
+VALUES ('billybob', '8781213455');
+
+-- This insert would result in an error:
+INSERT INTO contacts (name, phone)
+VALUES ('billybob', '8781213455');
+
+CREATE TABLE users (
+	username VARCHAR(20) NOT NULL,
+    age INT CHECK (age > 0)
+);
+
+CREATE TABLE palindromes (
+  word VARCHAR(100) CHECK(REVERSE(word) = word)
+);
+
+CREATE TABLE users2 (
+    username VARCHAR(20) NOT NULL,
+    age INT,
+    CONSTRAINT age_not_negative CHECK (age >= 0)
+);
+
+CREATE TABLE houses (
+  purchase_price INT NOT NULL,
+  sale_price INT NOT NULL,
+  CONSTRAINT sprice_gt_pprice CHECK(sale_price >= purchase_price)
+);
+
+ALTER TABLE companies 
+ADD COLUMN phone VARCHAR(15);
+
+ALTER TABLE companies
+ADD COLUMN employee_count INT NOT NULL DEFAULT 1;
+
+ALTER TABLE companies DROP COLUMN phone;
+
+RENAME TABLE companies to suppliers;
+
+ALTER TABLE suppliers RENAME TO companies;
+
+ALTER TABLE companies
+RENAME COLUMN name TO company_name;
+
+ALTER TABLE companies
+MODIFY company_name VARCHAR(100) DEFAULT 'unknown';
+
+ALTER TABLE suppliers
+CHANGE business biz_name VARCHAR(50);
