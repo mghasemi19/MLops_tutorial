7bff76c6d0ade7cf2d09f33592f77b8068a621eb
Meisam Ghasemi <meisamghasemi@ymail.com>
2026-02-09 15:17:17 +0330

    Add Flask files for MLOps project

---
+++ Flask/api.py
@@ -0,0 +1,67 @@
+### Put and Delete-HTTP Verbs
+### Working With API's--Json
+
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+## Initial Data in my to do list
+items = [
+    {"id": 1, "name": "Item 1", "description": "This is item 1"},
+    {"id": 2, "name": "Item 2", "description": "This is item 2"}
+]
+
+@app.route('/')
+def home():
+    return "Welcome To The Sample To DO List App"
+
+## 1. Get: Retrieve all the items
+@app.route('/items',methods=['GET'])
+def get_items():
+    return jsonify(items)
+
+## 2. Get: Retireve a specific item by Id
+@app.route('/items/<int:item_id>',methods=['GET'])
+def get_item(item_id):
+    item=next((item for item in items if item["id"]==item_id), None)     # Give me the first matching item, or None if there is none
+    if item is None:
+        return jsonify({"error":"item not found"})
+    return jsonify(item)
+
+## 3. Post :create a new task- API
+@app.route('/items',methods=['POST'])
+def create_item():
+    if not request.json or not 'name' in request.json:
+        return jsonify({"error":"item not found"})
+    new_item={
+        "id": items[-1]["id"] + 1 if items else 1,
+        #"name":request.json['name'],
+        "name":request.json.get('name'),
+        #"description":request.json["description"]
+        "description": request.json.get("description", "")
+    }
+    items.append(new_item)
+    #return jsonify(new_item)
+    return jsonify(items)
+
+# 4. Put: Update an existing item
+@app.route('/items/<int:item_id>',methods=['PUT'])
+def update_item(item_id):
+    item = next((item for item in items if item["id"] == item_id), None)
+    if item is None:
+        return jsonify({"error": "Item not found"})
+    #item['name'] = request.json.get('name', item['name'])
+    item['name'] = request.json.get('name')
+    #item['description'] = request.json.get('description', item['description'])
+    item['description'] = request.json.get('description')
+    return jsonify(item)
+
+# 5. DELETE: Delete an item
+@app.route('/items/<int:item_id>', methods=['DELETE'])
+def delete_item(item_id):
+    global items
+    items = [item for item in items if item["id"] != item_id]
+    return jsonify({"result": "Item deleted"})
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ Flask/main.py
@@ -0,0 +1,32 @@
+from flask import Flask,render_template, request
+'''
+ It creates an instance of the Flask class, 
+ which will be your WSGI (Web Server Gateway Interface) application.
+'''
+###WSGI Application
+app=Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return "<html><H1>Welcome to the flask course</H1></html>"
+
+@app.route("/index")
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # Accessing data hidden in the form body
+        username = request.form.get('name')
+        return f"Welcome back, {username}!"
+    
+    # If it's a GET request, just show the login page
+    return render_template('form.html')
+
+if __name__=="__main__":
+    app.run(debug=True)
